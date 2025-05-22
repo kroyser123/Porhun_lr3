f3ef388d51a26a5aac061c70eea09142dce3b6b0
kroyser <andrejporhunov990@gmail.com>
2025-05-22 16:40:49 +0300

    hi2323222

--- porhunlr3.cpp
+++ porhunlr3.cpp
@@ -11,17 +11,20 @@ char inputChar() {
 
 // Функция для вывода символа
 void printChar(char ch) {
-    
+    cout << "Введённый символ: " << ch 
+         << " (код: " << static_cast<int>(ch) << ")" << endl;
 }
 
 // Функция для вывода предыдущего символа
 void printPrevChar(char ch) {
-    
+    cout << "Предыдущий символ: " << static_cast<char>(ch - 1)
+         << " (код: " << static_cast<int>(ch - 1) << ")" << endl;
 }
 
 // Функция для вывода следующего символа
 void printNextChar(char ch) {
-    
+    cout << "Следующий символ: " << static_cast<char>(ch + 1)
+         << " (код: " << static_cast<int>(ch + 1) << ")" << endl;
 }
 
 int main() {
